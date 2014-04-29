dbadebbf1cef39d4eb9b81db7681e3a462f95d1c
kalyanvallamsetla <kalyan@10.185.195.20>
2014-04-29 15:46:56 -0700

    orginal

---
+++ grails-app/domain/course_rating/Courses.groovy
@@ -0,0 +1,12 @@
+package course_rating
+
+class Courses {
+
+	String Title
+	String Description
+	Integer Course_id
+	
+	
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/course_rating/Ratings.groovy
@@ -0,0 +1,12 @@
+package course_rating
+
+class Ratings {
+
+	Integer course_id
+	Integer student_id
+	Integer rating
+	String comments 
+	
+    static constraints = {
+    }
+}
